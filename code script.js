52aa184b94817b510a89caab1b109ee3ac010e1e
Epsipha-M <epsipham@gmail.com>
2024-07-31 22:40:56 +0530

    Add files via upload

---
+++ code script.js
@@ -0,0 +1,12 @@
+function updateOutput() {
+  let htmlCode = document.getElementById("Html-code");
+  let cssCode = document.getElementById("Css-code");
+  let jsCode = document.getElementById("Java script");
+
+  let output = document.getElementById("output");
+
+  output.contentDocument.body.innerHTML =
+    htmlCode.value + "<style>" + cssCode.value + "</style>";
+
+  output.contentWindow.eval(jsCode.value)
+}
